54f5d16145cca3a87315b664cee5cfa3b7fb06df
Ilya Tiunov <tiunov.ilya@gmail.com>
2017-09-30 23:24:27 +0500

    fix

--- FlashMapper/Internal/Implementations/GeneratingMappings/MappingExpressionAutocompleteService.cs
+++ FlashMapper/Internal/Implementations/GeneratingMappings/MappingExpressionAutocompleteService.cs
@@ -2,8 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Reflection;
-using System.Reflection.Emit;
 using FlashMapper.Models;
 using FlashMapper.Services.GeneratingMappings;
 using FlashMapper.Services.ParsingUserInput;
